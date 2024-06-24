a8f50a8ab5ab9152750140841e9d8e4d90a35bc4
Falgun-Mahajan-Avisoft <falgun.mahajan@avisoft.io>
2024-06-24 18:06:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ReactFlow, { Background, Controls } from 'reactflow';
+import 'reactflow/dist/style.css';
+// import './App.css'
+
+function App() {
+
+  return (
+   <ReactFlow>
+    <Background variant='dots'/>
+    <Controls/>
+   </ReactFlow>
+  )
+}
+
+export default App
